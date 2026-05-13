3b1e49cf05b60e31173142e7c27ee35d0f4999f1
杨栋琛 <15255032+yang-dongch@user.noreply.gitee.com>
2026-05-13 19:10:57 +0800

    chore: 移除 App.vue 中的冗余动画样式

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,3 @@ import { RouterView } from 'vue-router'
     </transition>
   </RouterView>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active { transition: opacity 0.3s ease; }
-.fade-enter-from,
-.fade-leave-to { opacity: 0; }
-</style>
